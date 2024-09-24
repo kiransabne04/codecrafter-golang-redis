77bf2d16b01b4c9c56a89c355e9a8bcfa3ab0d33
kiransabne04 <94209147+kiransabne04@users.noreply.github.com>
2024-09-24 12:42:15 +0530

    bind to a port

--- app/server.go
+++ app/server.go
@@ -20,22 +20,10 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err := l.Accept()
+	_, err = l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	// Uncomment this block to pass the first stage
-	//
-	// l, err := net.Listen("tcp", "0.0.0.0:6379")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 6379")
-	// 	os.Exit(1)
-	// }
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
 }
