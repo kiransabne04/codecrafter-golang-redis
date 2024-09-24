1749e594d9e168cad95927bece150dd32cc0cb59
kiransabne04 <94209147+kiransabne04@users.noreply.github.com>
2024-09-25 01:05:24 +0530

    initial replication ID & replication offset

--- app/server.go
+++ app/server.go
@@ -59,7 +59,8 @@ func NewServer() *Server {
 
 	server.Stats = ServerStats{}
 	server.DataStore = make(map[string]*Record)
-	
+	server.MasterReplid = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb"
+	server.MasterReplOffset = 0
 	return server
 }
 
