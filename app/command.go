1eb0d511d7d41d01876885bce05c594bea3c71c2
kiransabne04 <94209147+kiransabne04@users.noreply.github.com>
2024-09-25 09:13:01 +0530

    handshake 2 - REPLCONF for the slave to master server

--- app/command.go
+++ app/command.go
@@ -205,12 +205,64 @@ func (s *Server)HandShakeCommand() {
 		fmt.Println("-ERR couldnt connect to master at "+ address )
 	}
 	defer m.Close()
-	m.Write([]byte("*1\r\n$4\r\nPING\r\n"))
-	//time.Sleep(1 * time.Second)
-	m.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n6380\r\n"))
-	//return fmt.Sprintf("*%d\r\n$%d\r\n%s\r\n", 1, 4, "PING")
-	//time.Sleep(1 * time.Second)
-	m.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n"))
+	// sned PING to the master
+	_, err = m.Write([]byte("*1\r\n$4\r\nPING\r\n"))
+	if err != nil {
+		fmt.Println("-ERR sending ping to master server failed")
+		return
+	}
+
+	// read response from the master for ping command
+	response := make([]byte, 2048)
+	_, err = m.Read(response)
+	if err != nil {
+		fmt.Println("-ERR reading PONG response from master failed", err)
+		return
+	}
+	fmt.Println("received from master -> ", string(response))
+
+	//check if response is PONG
+	if string(response[:6]) != "+PONG\r\n" {
+		fmt.Println("-ERR invalid response from master")
+		return
+	}
+
+	//send Replconf listening port to master
+	_, err = m.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n6380\r\n"))
+	if err != nil {
+		fmt.Println("-ERR sneding REPLCONF (listening-port) failed")
+		return
+	}
+
+	// read replconf response
+	_, err = m.Read(response)
+	if err != nil {
+		fmt.Println("-ERR readng REPLCONF response from master failed", err)
+		return
+	}
+	fmt.Println("received REPLCONF (listening-port) response ", string(response))
+
+	// send REPLCONF capa psync2 to master
+	_, err = m.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n"))
+	if err != nil {
+		fmt.Println("-ERR sendging REPLCONF (capa psync2) failed")
+		return
+	}
+
+	// read REPLCONF capa psync2 response
+	_, err = m.Read(response)
+	if err != nil {
+		fmt.Println("-ERR reading REPLCONF (capa psync2) response ", err)
+		return
+	}
+	fmt.Println("Received REPLCONF (capa psync2) response ", string(response))
+
+	// //time.Sleep(1 * time.Second)
+	// m.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n6380\r\n"))
+	// //return fmt.Sprintf("*%d\r\n$%d\r\n%s\r\n", 1, 4, "PING")
+	// //time.Sleep(1 * time.Second)
+	// m.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n"))
+
 }
 
 
