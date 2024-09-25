6be9d1a86425749de78ca1263d0b77be34bd1f7a
kiransabne04 <94209147+kiransabne04@users.noreply.github.com>
2024-09-25 09:54:36 +0530

    handshake 3 response handling for  psync command

--- app/command.go
+++ app/command.go
@@ -131,7 +131,7 @@ func (s *Server) ReplConfCommand(c net.Conn, args []string) string {
 // reponse for PSYNC command received from slave to master
 func (s *Server)FullResyncCommand(c net.Conn, args[] string) string {
 	fmt.Println("full resync command received:", args)
-	if len(args) > 1 {
+	if len(args) > 0 {
 		return "-ERR wrong number of 'PSYNC' arguments\r\n"
 	}
 	return fmt.Sprintf("+FULLRESYNC %s 0\r\n", s.MasterReplid)
