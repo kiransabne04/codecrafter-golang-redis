47d30db463968afae676a05ce0ca749f827fec16
kiransabne04 <94209147+kiransabne04@users.noreply.github.com>
2024-09-25 14:20:30 +0530

    single replica propagate command

--- app/replication.go
+++ app/replication.go
@@ -91,7 +91,7 @@ func (s *Server)HandShakeCommand() {
 	}
 	s.ConnectedReplica = m
 	fmt.Println("s.ConnectedReplica -> ", s.ConnectedReplica, s.Listener)
-	fmt.Println("recevied PSYNC response :", string(response))
+	fmt.Println("recevied PSYNC response :", string(response), s.ConnectedReplica)
 }
 
 
