03a796e43d730aeaa1d60d085b0a9865476922a1
kiransabne04 <94209147+kiransabne04@users.noreply.github.com>
2024-09-26 16:32:11 +0530

    command processing

--- app/replication.go
+++ app/replication.go
@@ -91,7 +91,8 @@ func (s *Server)HandShakeCommand() () {
 
 func (s *Server) replicateMainLoop(conn net.Conn) {
 	reader := bufio.NewReader(conn)
-
+	line, _ := reader.Peek(100)
+	fmt.Println("replicate main loop line -> ", line)
 	for {
 		// parse incoming command from the master
 		inputCmd, err := parseCommand(reader)
